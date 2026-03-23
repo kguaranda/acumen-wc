2bd7991352db58f7dfc3492d7b68abd293eb6596
Kristian Johansen <contact@kristianjohansen.dk>
2026-03-23 14:42:59 +0100

    add sass

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Landing page</div>
+</template>
+
+<style lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "nuxt-gtag",
+  ],
+  // @ts-ignore
+  gtag: {
+    id: "G-V0LRFNH5BY",
+  },
+  ssr: false,
+  // FONTS GOES HERE
+
+  // Vite
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/scss/_globals.scss" as *;
+          `,
+        },
+      },
+    },
+  },
+
+  css: ["~/assets/scss/_main.scss"],
+});
