2bf10417cebfb2b8133dffdbb82620fdec069f61
Kristian Johansen <contact@kristianjohansen.dk>
2026-04-10 13:59:25 +0200

    add updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,20 @@ export default defineNuxtConfig({
         },
       ],
       link: [
-        { rel: "icon", sizes: "32x32", href: "/favicon.ico" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
         { rel: "stylesheet", href: "https://use.typekit.net/pzv2tcy.css" },
       ],
     },
